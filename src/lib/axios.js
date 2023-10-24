a67b873b4c0806244465f3704dd8546e8f2c2b51
RamFarid <workprojects22@gmail.com>
2023-10-24 20:45:00 +0300

    return URL base

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,8 +2,8 @@ import axiosInst from 'axios'
 
 const axios = axiosInst.create({
   baseURL: import.meta.env.PROD
-    ? 'http://localhost:5000/'
-    : 'http://localhost:5000/',
+    ? 'https://omanaakfa-server.vercel.app'
+    : 'http://localhost:5000',
   headers: {
     Authorization: localStorage.getItem('creds') || '',
   },
