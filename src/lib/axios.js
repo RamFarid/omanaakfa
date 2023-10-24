9881bb2c66506c5aeccefd196c7c1abb7a04a2a2
RamFarid <workprojects22@gmail.com>
2023-10-24 16:46:40 +0300

    solve pp

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axiosInst from 'axios'
 
 const axios = axiosInst.create({
   baseURL: import.meta.env.PROD
-    ? 'https://mo2tamr-omnaakfa.vercel.app'
+    ? 'https://omanaakfa-server.vercel.app'
     : 'http://localhost:5000',
   headers: {
     Authorization: localStorage.getItem('creds') || '',
