e9627d9275630aa0394781b15b0b80d00089b37a
RamFarid <workprojects22@gmail.com>
2023-10-24 22:14:15 +0300

    change the server uRl

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axiosInst from 'axios'
 
 const axios = axiosInst.create({
   baseURL: import.meta.env.PROD
-    ? 'https://omanaakfa-server.vercel.app'
+    ? 'https://omanaafka-server.onrender.com'
     : 'http://localhost:5000',
   headers: {
     Authorization: localStorage.getItem('creds') || '',
